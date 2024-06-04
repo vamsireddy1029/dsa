8ee41ae55e7941ff8c4e71968e2645c12e5a59c6
vamsireddy1029 <127970673+vamsireddy1029@users.noreply.github.com>
2024-06-04 17:36:17 +0530

    Add files via upload

---
+++ Task-4/public/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import TodoList from './TodoList';
+
+const App = () => {
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
+};
+
+export default App;
