4a05a7ddb8c9f34d597cc6b1fc1474cc00192f10
vamsireddy1029 <127970673+vamsireddy1029@users.noreply.github.com>
2024-06-21 22:18:26 +0530

    Create App.js

---
+++ public/src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import ImageGallery from './ImageGallery';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Image Gallery</h1>
+      <ImageGallery />
+    </div>
+  );
+};
+
+export default App;
